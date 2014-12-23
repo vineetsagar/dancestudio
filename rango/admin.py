def33e57ead430d8728f90333bb9ec2505735e3e
Vineet Sagar <sagar.vineet@gmail.com>
2014-12-23 22:14:10 +0530

    removing rango files from repo

--- rango/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from rango.models import Members, EventCategory, EventType
-
-# Register your models here.
-
-admin.site.register(Members)
-admin.site.register(EventCategory)
-admin.site.register(EventType)
