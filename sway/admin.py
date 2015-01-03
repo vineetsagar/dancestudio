6b7144cb4117a7f6790cfb351d469ae6aef15474
Balwinder Kumar <balwinder.kumar@outlook.com>
2015-01-03 13:00:03 +0530

    Committing the customized admin models for search capability
    Committing the customized admin models for search capability

--- sway/admin.py
+++ sway/admin.py
@@ -3,10 +3,24 @@ from django.contrib import admin
 from sway.models import Members, EventCategory, EventType, Events, \
     EventOccurence
 
+class MembersAdmin(admin.ModelAdmin):
+	search_fields = ['first_name','last_name','email',]
 
+class EventCategoryAdmin(admin.ModelAdmin):
+	search_fields = ['event_category_name']
+
+class EventTypeAdmin(admin.ModelAdmin):
+	search_fields = ['event_type_name']
+
+class EventsAdmin(admin.ModelAdmin):
+	search_fields = ['event_name']
+
+class EventOccurenceAdmin(admin.ModelAdmin):
+	pass
+					
 # Register your models here.
-admin.site.register(Members)
-admin.site.register(EventCategory)
-admin.site.register(EventType)
-admin.site.register(Events)
-admin.site.register(EventOccurence)
+admin.site.register(Members,MembersAdmin)
+admin.site.register(EventCategory,EventCategoryAdmin)
+admin.site.register(EventType,EventTypeAdmin)
+admin.site.register(Events,EventsAdmin)
+admin.site.register(EventOccurence,EventOccurenceAdmin)
