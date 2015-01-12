39b2bb03008e0c1d73b0daf8a8e2a5f1c120ef6b
Balwinder Kumar <balwinder.kumar@outlook.com>
2015-01-12 22:08:35 +0530

    Committing the Settings config as all are using MySQL with common settings
    If any one needs change he can do it locally.

--- dancestudio/settings.py
+++ dancestudio/settings.py
@@ -71,8 +71,12 @@ WSGI_APPLICATION = 'dancestudio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'sway',
+        'USER': 'sway',
+        'PASSWORD': 'sway',
+        'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
