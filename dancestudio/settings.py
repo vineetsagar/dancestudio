9a453b48ef4949b5e96b007e29c5cd0d7406aa41
Balwinder Kumar <balwinder.kumar@outlook.com>
2015-08-15 22:16:27 +0530

    committing login url

--- dancestudio/settings.py
+++ dancestudio/settings.py
@@ -160,7 +160,7 @@ STATICFILES_DIRS = (
 os.path.join(BASE_DIR, 'static'),
 )
 
-LOGIN_URL='/sway/members/login'
+LOGIN_URL='/sway/login'
 '''
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.request',
