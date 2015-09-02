d423265efcd0ccb82ec7d2ed590e8725645c1135
Balwinder Kumar <balwinder.kumar@outlook.com>
2015-09-02 23:27:01 +0530

    committing the middleware sequence change

--- dancestudio/settings.py
+++ dancestudio/settings.py
@@ -64,12 +64,12 @@ PUSH_NOTIFICATIONS_SETTINGS = {
 }
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'sway.middleware.activate_tz.ActivateTimeZone',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'sway.middleware.activate_tz.ActivateTimeZone',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.locale.LocaleMiddleware'
