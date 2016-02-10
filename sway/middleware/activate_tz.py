be7fcd9968c921c62f08cbe2f5d7018276a8c337
Vineet Sagar <sagar.vineet@gmail.com>
2016-02-10 21:55:19 +0530

    remove pytz import from the file

--- sway/middleware/activate_tz.py
+++ sway/middleware/activate_tz.py
@@ -1,4 +1,4 @@
-import pytz
+#import pytz
 
 from django.utils import timezone
 
@@ -9,7 +9,7 @@ class ActivateTimeZone(object):
         if hasattr(request,'user'):
         	if hasattr(request.user, 'studiouser'):
         		tzname = request.user.studiouser.studio_id.timezone
-        if tzname:
-            timezone.activate(pytz.timezone(tzname))
-        else:
-            timezone.deactivate()
+        #if tzname:
+         #   timezone.activate(pytz.timezone(tzname))
+        #else:
+         #   timezone.deactivate()
